fc1a92887127cf8ca15d5afe53b2f92ea60af974
Yuta <y.sugiyama54@gmail.com>
2020-02-04 22:53:28 +0900

    Todoテーブルにダミーデータ挿入のためのfactoryとseederの作成

--- server/database/migrations/2020_02_03_173411_create_todos_table.php
+++ server/database/migrations/2020_02_03_173411_create_todos_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTodoTable extends Migration
+class CreateTodosTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateTodoTable extends Migration
      */
     public function up()
     {
-        Schema::create('todo', function (Blueprint $table) {
+        Schema::create('todos', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
             $table->text('description');
@@ -29,6 +29,6 @@ class CreateTodoTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('todo');
+        Schema::dropIfExists('todos');
     }
 }
